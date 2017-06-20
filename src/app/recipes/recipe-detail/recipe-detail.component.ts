b6598e12bc614d8f9b1434d4707707315f3d9028
Ramana <ramanakellampalli@gmail.com>
2017-06-20 19:26:24 -0400

    Updated buttons on new and edit recipe components

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -30,8 +30,12 @@ export class RecipeDetailComponent implements OnInit {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
   onEditRecipe() {
-    this.router.navigate(['edit'], {relativeTo:this.route});
+    this.router.navigate(['edit'], {relativeTo: this.route});
     // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
+  }
+
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
   }
 
 }
