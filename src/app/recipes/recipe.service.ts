5b43fb631d8850cbf7388e7d746f356c0bf51224
Ramana <ramanakellampalli@gmail.com>
2017-06-24 16:23:06 -0400

    Changed recipe.service.ts

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,16 +11,16 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'Tasty Schnitzel',
+      'Chicken Biryani',
       'A super-tasty Schnitzel - just awesome!',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      'https://i.ytimg.com/vi/ef0iG3txMbY/maxresdefault.jpg',
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
       ]),
-    new Recipe('Big Fat Burger',
+    new Recipe('Kodi Kura',
       'What else you need to say?',
-      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      'https://i.ytimg.com/vi/tzUcYpgRD78/maxresdefault.jpg',
       [
         new Ingredient('Buns', 2),
         new Ingredient('Meat', 1)
